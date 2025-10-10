ace74e5aa56df438809a54704e4347e926139c56
Pawel2923 <pawelporemba123@gmail.com>
2025-10-10 22:26:02 +0200

    feat: enhance dark theme styles and improve Header component layout

--- src/renderer/src/components/Header.tsx
+++ src/renderer/src/components/Header.tsx
@@ -3,14 +3,16 @@ import { TypographyH1 } from './ui/typography'
 import { NavLink } from 'react-router'
 
 export default function Header(): React.ReactNode {
+  const navLinkClasses = 'text-text-disabled hover:text-text-primary-hover transition'
+
   return (
-    <header>
+    <header className="flex gap-2 py-6 justify-center items-center w-full">
       <TypographyH1>
-        <NavLink to="/" className="text-text-disabled hover:text-text-primary-hover">
+        <NavLink to="/" className={navLinkClasses}>
           Szyfrowanie
         </NavLink>{' '}
         <span className="text-text-subtle">/</span>{' '}
-        <NavLink to="/decrypt" className="text-text-disabled hover:text-text-primary-hover">
+        <NavLink to="/decrypt" className={navLinkClasses}>
           Odszyfrowywanie
         </NavLink>
       </TypographyH1>
