6a5d7e24000c961396b51f3b6aefa9374aebae9a
Pawel2923 <pawelporemba123@gmail.com>
2025-10-10 20:19:06 +0200

    feat: install react-router and add dummy routes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,20 @@
+import { Link, Route, Routes } from 'react-router'
+
 function App(): React.JSX.Element {
-  return <></>
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div>
+            <p>Hello World</p>
+            <Link to="/about">About</Link>
+          </div>
+        }
+      />
+      <Route path="/about" element={<p>About Us</p>} />
+    </Routes>
+  )
 }
 
 export default App
