6ecc39067ed9eb84e9a205b562cdd914feb54c24
Pawel2923 <pawelporemba123@gmail.com>
2025-10-11 10:38:04 +0200

    refactor: use constants for classes instead of component with conditional rendering

--- src/renderer/src/components/home/FileContainer.tsx
+++ src/renderer/src/components/home/FileContainer.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import DataInputContainer from './DataInputContainer'
+import React, { useId } from 'react'
 import { FileUp } from 'lucide-react'
 import { TypographyLabel } from '../ui/typography'
+import { FILE_CONTAINER_CLASSES } from './constants'
 
-export default function FileContainer(): React.ReactNode {
+export default function FileContainer({
+  ...rest
+}: React.HTMLAttributes<HTMLDivElement>): React.ReactNode {
+  const labelId = useId()
+
   return (
-    <DataInputContainer containerType="file">
-      <FileUp size={64} aria-hidden="true" />
-      <TypographyLabel>Wybierz plik z dysku</TypographyLabel>
-    </DataInputContainer>
+    <div className={FILE_CONTAINER_CLASSES} {...rest}>
+      <input
+        type="file"
+        className="absolute top-0 left-0 w-full h-full opacity-0"
+        aria-labelledby={labelId}
+      />
+      <div id={labelId} className="flex flex-col justify-center items-center pointer-events-none">
+        <FileUp size={64} aria-hidden="true" />
+        <TypographyLabel>Wybierz plik z dysku</TypographyLabel>
+      </div>
+    </div>
   )
 }
--- src/renderer/src/components/home/TextContainer.tsx
+++ src/renderer/src/components/home/TextContainer.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import DataInputContainer from './DataInputContainer'
 import { Type } from 'lucide-react'
 import { TypographyLabel } from '../ui/typography'
+import { Button } from '../ui/button'
+import { CONTAINER_BASE_CLASSES } from './constants'
 
-export default function TextContainer(): React.ReactNode {
+export default function TextContainer({
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement>): React.ReactNode {
   return (
-    <DataInputContainer containerType="text">
+    <Button className={CONTAINER_BASE_CLASSES} variant="none" size="none" {...rest}>
       <Type size={64} aria-hidden="true" />
       <TypographyLabel>Wpisz lub wklej tekst</TypographyLabel>
-    </DataInputContainer>
+    </Button>
   )
 }
