33c9933f96b85284fd69207bf405d753f74d025e
Pawel2923 <pawelporemba123@gmail.com>
2025-10-13 21:37:13 +0200

    feat: enhance ResultPage layout with header and main components

--- src/renderer/src/pages/result.tsx
+++ src/renderer/src/pages/result.tsx
@@ -1,6 +1,13 @@
+import Header from '@renderer/components/Header'
+import Main from '@renderer/components/Main'
 import { Button } from '@renderer/components/ui/button'
+import { TypographyH1 } from '@renderer/components/ui/typography'
+import { ArrowDownToLine, ExternalLink } from 'lucide-react'
+import { useNavigate } from 'react-router'
 
 export default function ResultPage(): React.ReactNode {
+  const navigate = useNavigate()
+
   const downloadResult = async (): Promise<void> => {
     try {
       const download = await window.api.file.download()
@@ -31,19 +38,32 @@ export default function ResultPage(): React.ReactNode {
     try {
       await window.api.file.clear()
       console.log('File cleared successfully')
+      navigate('/')
     } catch (error) {
       console.error('Error clearing file:', error)
     }
   }
 
   return (
-    <div>
-      <p>Wynik</p>
-      <Button onClick={downloadResult}>Pobierz</Button>
-      <Button onClick={previewResult}>Podgląd</Button>
-      <Button onClick={clearFile} variant="destructive">
-        Wyczyść plik
-      </Button>
-    </div>
+    <>
+      <Header>
+        <TypographyH1>Wynik operacji</TypographyH1>
+      </Header>
+      <Main className="flex-col gap-6">
+        <div className="flex gap-4">
+          <Button onClick={downloadResult}>
+            <ArrowDownToLine aria-hidden="true" />
+            Pobierz
+          </Button>
+          <Button onClick={previewResult} variant="outline">
+            <ExternalLink aria-hidden="true" />
+            Podgląd
+          </Button>
+        </div>
+        <Button onClick={clearFile} variant="destructive">
+          Powróć do ekranu głównego
+        </Button>
+      </Main>
+    </>
   )
 }
