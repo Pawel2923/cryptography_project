e876cbc5f22afbbf7e2c9e54a71725a52a0de83a
Pawel2923 <pawelporemba123@gmail.com>
2025-10-14 14:15:48 +0200

    feat: add vigenere cipher component and data

---
+++ src/renderer/src/components/algorithm/VigenereCipher.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Field, FieldLabel } from '../ui/field'
+import { Input } from '../ui/input'
+import { TypographyLabel } from '../ui/typography'
+import { CipherProps } from './cipher-props'
+
+export default function VigenereCipher({
+  description,
+  setKey,
+  setIsValid
+}: CipherProps): React.ReactNode {
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value
+    setKey(value)
+    const isValid = /^[a-zA-Z]+$/.test(value) && value.length >= 2
+    setIsValid(isValid)
+  }
+
+  return (
+    <>
+      <Field className="text-center">
+        {description && <TypographyLabel>{description}</TypographyLabel>}
+        <FieldLabel htmlFor="vigenere-key">Klucz (litery)</FieldLabel>
+        <Input
+          type="text"
+          id="vigenere-key"
+          minLength={2}
+          placeholder="Wrowadź klucz"
+          onChange={changeHandler}
+        />
+      </Field>
+    </>
+  )
+}
