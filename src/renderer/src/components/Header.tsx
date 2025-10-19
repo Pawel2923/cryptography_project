de778380ae4866ae86d1a8a0b3d2d043d600af4d
Pawel2923 <pawelporemba123@gmail.com>
2025-10-19 10:43:07 +0200

    feat: add select for changing operation in algorithms page

--- src/renderer/src/components/Header.tsx
+++ src/renderer/src/components/Header.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 type HeaderProps = {
   children: React.ReactNode
+  className?: string
 }
 
-export default function Header({ children }: HeaderProps): React.ReactNode {
-  return <header className="flex gap-2 py-6 justify-center items-center w-full">{children}</header>
+export default function Header({ children, className }: HeaderProps): React.ReactNode {
+  return (
+    <header className={`flex gap-2 py-6 justify-center items-center w-full ${className}`}>
+      {children}
+    </header>
+  )
 }
--- src/renderer/src/pages/algorithms.tsx
+++ src/renderer/src/pages/algorithms.tsx
@@ -1,11 +1,18 @@
 import AlgorithmContainer from '@renderer/components/algorithms/AlgorithmContainer'
 import { BackButton } from '@renderer/components/BackButton'
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem
+} from '@renderer/components/ui/select'
 import Header from '@renderer/components/Header'
 import Main from '@renderer/components/Main'
 import { TypographyH1 } from '@renderer/components/ui/typography'
 import { useTitle } from '@renderer/hooks/useTitle'
 import { getAlgorithms } from '@renderer/lib/algorithm-util'
-import React from 'react'
+import React, { useState } from 'react'
 
 const algorithms = getAlgorithms()
 
@@ -14,17 +21,38 @@ export default function AlgorithmsPage({
 }: {
   operation: 'encrypt' | 'decrypt'
 }): React.ReactNode {
-  useTitle(`Wybierz algorytm - ${operation === 'encrypt' ? 'Szyfrowanie' : 'Deszyfrowanie'}`)
+  const [selectedOperation, setSelectedOperation] = useState(operation)
+
+  useTitle(
+    `Wybierz algorytm - ${selectedOperation === 'encrypt' ? 'Szyfrowanie' : 'Deszyfrowanie'}`
+  )
+
+  const valueChangeHandler = (value: string): void => {
+    setSelectedOperation(value as 'encrypt' | 'decrypt')
+  }
 
   return (
     <>
       <BackButton />
-      <Header>
+      <Header className="flex-col">
         <TypographyH1>Wybierz algorytm</TypographyH1>
+        <Select defaultValue={selectedOperation} onValueChange={valueChangeHandler}>
+          <SelectTrigger>
+            <SelectValue placeholder="Wybierz operację" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="encrypt">Szyfrowanie</SelectItem>
+            <SelectItem value="decrypt">Deszyfrowanie</SelectItem>
+          </SelectContent>
+        </Select>
       </Header>
       <Main layout="grid" className="grid-cols-3">
         {algorithms.map((alg) => (
-          <AlgorithmContainer to={`/${operation}/algorithm/${alg.id}`} key={alg.id} icon={alg.icon}>
+          <AlgorithmContainer
+            to={`/${selectedOperation}/algorithm/${alg.id}`}
+            key={alg.id}
+            icon={alg.icon}
+          >
             {alg.name}
           </AlgorithmContainer>
         ))}
